906762e865a8f81bff39cfd3c4b8008617bdb3b1
Barschedule <leovtimes17@gmail.com>
2026-05-07 21:40:02 -0500

    Update app2.js

--- app2.js
+++ app2.js
@@ -1,3 +1,4 @@
+window.onerror=function(m,s,l){document.getElementById('sp-err').style.display='block';document.getElementById('sp-err').textContent='JS Error: '+m+' line:'+l;};
 “use strict”;
 var DEFAULT_PIN=“1234”;
 var SUPER_ADMIN=“Fabinho”;
